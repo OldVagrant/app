15f9367d0ab83caa3bffaf981a28c58f9fed0e33
OldVagrant <vagrantnovice@gmail.com>
2020-06-09 17:20:03 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,44 +1,21 @@
-$(document).ready(function () {
+function plus(){
+var a, b, res;
+    a = document.getElementById('n1').value;
+    a = parseInt(a);
 
-    // VARIABLES
-    var calc = $('.calculator');
-    var calcDisplay = calc.find('.calculator__display');
-    var calcKeys = calc.find('.calculator__key');
-    var calcButton = calc.find('.calculator__button');
-    var calcClear = calc.find('.calculator__clear');
-    var calcEqual = calc.find('.calculator__key--equal');
-    var calcPower = calc.find('.calculator__power');
-    var calcSpace = calc.find('.calculator__backspace');
+    b = document.getElementById('n2').value;
+    a = parseInt(b);
+    res = a+b;
+    document.getElementById(out).innerHTML = res;
+}
 
-    // INIT CALC KEYS
-    calcKeys.each(function () {
-        var current = $(this).attr('value');
-        $(this).text(current);
-    });
+function minus(){
+var a, b, res;
+    a = document.getElementById('n1').value;
+    a = parseInt(a);
 
-    // ADD NUMBERS TO INPUT
-    calcButton.on('click', function () {
-        calcDisplay.val( calcDisplay.val() + $(this).attr('value') );
-    });
-
-    // CLEAR INPUT
-    calcClear.on('click', function () {
-        calcDisplay.val('');
-    });
-
-    // SHOW RESULT
-    calcEqual.on('click', function () {
-        calcDisplay.val( eval( calcDisplay.val() ) );
-    });
-
-    // POWER BUTTON
-    calcPower.on('click', function () {
-        calcDisplay.val( Math.pow( calcDisplay.val(), 3 ) );
-    });
-
-    // BACKSPACE BUTTON
-    calcSpace.on('click', function () { // http://www.w3schools.com/jsref/jsref_substring.asp
-        calcDisplay.val( calcDisplay.val().substring(0, calcDisplay.val().length-1) );
-    });
-
-});
+    b = document.getElementById('n2').value;
+    a = parseInt(b);
+    res = a-b;
+    document.getElementById(out).innerHTML = res;
+}
